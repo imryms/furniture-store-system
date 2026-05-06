75a5c3034638c72573fe6e5099843075f3c1b2bd
Maryam Altammam <mrymst03@gmail.com>
2026-05-06 14:35:29 +0300

    set up

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Branch, User
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Branch', {'fields': ('branch',)}),
+    )
+
 admin.site.register(Branch)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
