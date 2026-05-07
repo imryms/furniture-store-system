12a3cbb8a5d247286c57a191c2bdf9462af74e22
Maryam Altammam <mrymst03@gmail.com>
2026-05-07 09:47:28 +0300

    new change in models

--- store/admin.py
+++ store/admin.py
@@ -43,7 +43,6 @@ class StockAdmin(admin.ModelAdmin):
 
 admin.site.register(Branch)
 admin.site.register(User, CustomUserAdmin)
-
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductDetails)
 admin.site.register(Stock, StockAdmin)
