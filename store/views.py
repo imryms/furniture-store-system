714ad5440796fd4d5a97a07c9faa8428d8ca3ce0
Maryam Altammam <mrymst03@gmail.com>
2026-05-10 09:47:20 +0300

    dashboard view

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Product
 from .models import Order
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Customer, Order, ProductDetails
+from .models import Customer, Order, ProductDetails, Stock
 from .forms import CustomerForm
 from django.views.generic import (
     ListView,
@@ -11,21 +11,49 @@ from django.views.generic import (
     DeleteView,
     UpdateView,
 )
+from django.db.models import Count
 
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    today = timezone.now().date()
+    user_branch = request.user.branch
+
+    todays_orders = Order.objects.filter(
+        branch = user_branch,
+        created_at__date =today
+    ).order_by('-created_at')
+
+    low_stock = Stock.objects.filter(
+        branch=user_branch,
+        location_type = 'showroom',
+        quantity=0
+    )
+
+    top_products = ProductDetails.objects.filter(
+        orderitem__order__branch=user_branch
+    ).annotate(
+        order_count = Count('orderitem')
+    ).order_by('-order_count')[:3]
+
+    context = {
+        'todays_orders':todays_orders,
+        'todays_count': todays_orders.count(),
+        'low_stock': low_stock,
+        'top_products':top_products,
+    }
+    return render(request, 'dashboard.html', context)
+
 
 
 class ProductListView(ListView):
     model = Product
-    template_name: "products.html"
+    template_name= "products.html"
     context_object_name = "products"
 
 
 class OrderListView(ListView):
     model = Order
-    template_name: "orders/orders.html"
+    template_name= "orders/orders.html"
     context_object_name = "orders"
 
 
