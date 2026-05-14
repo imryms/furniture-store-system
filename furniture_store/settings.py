c35e9644f333a2c3748a84e7e62d22514e67720c
Maryam Altammam <mrymst03@gmail.com>
2026-05-14 09:12:16 +0300

    prepare for deployment

--- furniture_store/settings.py
+++ furniture_store/settings.py
@@ -136,5 +136,11 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
